12ee9b4e9e2bc0dcc9c11734c6639e0995be8f5a
Ahkeem Lang <langahkeem@gmail.com>
2016-08-02 17:46:13 -0500

    Created a gem called bcrypt for my app. Finished my users controller crud actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,8 +40,23 @@ class UsersController < ApplicationController
   end
 
   def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
+
+  private
+  def set_user
+    User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
